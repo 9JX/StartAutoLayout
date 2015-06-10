41e11bcf918b9eb15c82c5f06b7b0c59a58695d1
songjian <songjian@metao.com>
2015-06-10 10:44:15 +0800

    Initial Commit

---
+++ StartAutoLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StartAutoLayout
+//
+//  Created by songjian on 15/6/10.
+//  Copyright (c) 2015年 songjian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StartAutoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StartAutoLayout
+//
+//  Created by songjian on 15/6/10.
+//  Copyright (c) 2015年 songjian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
